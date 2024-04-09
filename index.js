bd0e856a2aaf7bcc670d0d6cda571463982505df
Eduardo <tkd13000@gmail.com>
2024-04-08 19:42:17 -0600

    board was finished

---
+++ index.js
@@ -0,0 +1,109 @@
+const tablero = document.getElementById('tablero');
+
+// Generar las 64 casillas del tablero
+for (let i = 0; i < 64; i++) {
+    const casilla = document.createElement('div');
+    casilla.classList.add('casilla');
+    // Determinar si la casilla es par o impar para aplicar el color correspondiente
+    // if (i % 2 === 0)
+    // {
+    //     casilla.classList.add('par');
+    // } 
+    // else 
+    // {
+    //     casilla.classList.add('impar');
+    // }
+
+    if ( (i % 2 === 0 && i < 8) || (i % 2 === 1 && i < 8) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('par');
+        } 
+        else
+        {
+            casilla.classList.add('impar');
+        }
+    }
+    else if ( (i % 2 === 0 && i < 16) || (i % 2 === 1 && i < 16) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('impar');
+        } 
+        else
+        {
+            casilla.classList.add('par');
+        }
+    }
+    else if ( (i % 2 === 0 && i < 24) || (i % 2 === 1 && i < 24) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('par');
+        } 
+        else
+        {
+            casilla.classList.add('impar');
+        }
+    }
+    else if ( (i % 2 === 0 && i < 32) || (i % 2 === 1 && i < 32) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('impar');
+        } 
+        else
+        {
+            casilla.classList.add('par');
+        }
+    }
+    else if ( (i % 2 === 0 && i < 40) || (i % 2 === 1 && i < 40) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('par');
+        } 
+        else
+        {
+            casilla.classList.add('impar');
+        }
+    }
+    else if ( (i % 2 === 0 && i < 48) || (i % 2 === 1 && i < 48) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('impar');
+        } 
+        else
+        {
+            casilla.classList.add('par');
+        }
+    }
+    else if ( (i % 2 === 0 && i < 56) || (i % 2 === 1 && i < 56) ) 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('par');
+        } 
+        else
+        {
+            casilla.classList.add('impar');
+        }
+    }
+    else 
+    {
+        if (i % 2 === 0)
+        {
+            casilla.classList.add('impar');
+        } 
+        else
+        {
+            casilla.classList.add('par');
+        }
+    }
+    
+    
+
+    tablero.appendChild(casilla);
+}
