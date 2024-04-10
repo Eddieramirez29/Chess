6f26cf33fbcb5ddd1576155ce3250de0c37438eb
Eduardo <tkd13000@gmail.com>
2024-04-09 20:56:45 -0600

    pawns in bottom were added

---
+++ pieces.js
@@ -0,0 +1,33 @@
+function agregarImagen() {
+    let contenedor = document.getElementById('tablero');
+    let left = 485;
+
+  
+  for(i = 0; i < 8;i++)
+  {
+    
+     // Crear un elemento de imagen
+     let imagen = document.createElement('img');
+     // Establecer la URL de la imagen
+     imagen.src = './piezas/chess_743907.png';
+      // Opcional: establecer atributos adicionales como el ancho y el alto
+    imagen.width = 50;
+    imagen.height = 50;
+     // Establecer estilos de posicionamiento
+    imagen.style.position = 'absolute';
+    imagen.style.top = '400px'; // Posición vertical
+    imagen.style.left = left.toString() + "px"; 
+
+    left = left + 50;
+   
+    // Adjuntar la imagen al contenedor en el DOM
+    
+    contenedor.appendChild(imagen);
+  }
+  
+  
+  
+    
+  }
+
+  agregarImagen() ;
