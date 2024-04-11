e1eba8f796409f0bab14ba05fb482fda40fab18b
Eduardo <tkd13000@gmail.com>
2024-04-10 20:06:30 -0600

    bishop added

--- pieces.js
+++ pieces.js
@@ -85,6 +85,65 @@ function agregarPeon() {
  
   }
 
+  function agregarTorre() {
+    let contenedor = document.getElementById('tablero');
+    let left = 485;
+
+  
+  for(i = 0; i < 2;i++)
+  {
+    
+     // Crear un elemento de imagen
+     let imagen = document.createElement('img');
+     // Establecer la URL de la imagen
+     imagen.src = './piezas/chess.png';
+      // Opcional: establecer atributos adicionales como el ancho y el alto
+    imagen.width = 50;
+    imagen.height = 50;
+     // Establecer estilos de posicionamiento
+    imagen.style.position = 'absolute';
+    imagen.style.top = '450px'; // Posición vertical
+    imagen.style.left = left.toString() + "px"; 
+
+    left = left + 350;
+   
+    // Adjuntar la imagen al contenedor en el DOM
+    
+    contenedor.appendChild(imagen);
+  }
+ 
+  }
+
+  function agregarAlfil() {
+    let contenedor = document.getElementById('tablero');
+    let left = 585;
+
+  
+  for(i = 0; i < 2;i++)
+  {
+    
+     // Crear un elemento de imagen
+     let imagen = document.createElement('img');
+     // Establecer la URL de la imagen
+     imagen.src = './piezas/bishop.png';
+      // Opcional: establecer atributos adicionales como el ancho y el alto
+    imagen.width = 50;
+    imagen.height = 50;
+     // Establecer estilos de posicionamiento
+    imagen.style.position = 'absolute';
+    imagen.style.top = '450px'; // Posición vertical
+    imagen.style.left = left.toString() + "px"; 
+
+    left = left + 150;
+   
+    // Adjuntar la imagen al contenedor en el DOM
+    
+    contenedor.appendChild(imagen);
+  }
+ 
+  }
+
+  agregarAlfil();
   agregarCaballo();
   agregarPeon();
   agregarTorre();
