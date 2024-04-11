6ed434d327e9d46cc9e7d9af2fd6573ef93ff9f1
Eduardo <tkd13000@gmail.com>
2024-04-10 19:54:57 -0600

    Towers were added

--- pieces.js
+++ pieces.js
@@ -1,4 +1,4 @@
-function agregarImagen() {
+function agregarPeon() {
     let contenedor = document.getElementById('tablero');
     let left = 485;
 
@@ -30,4 +30,37 @@ function agregarImagen() {
     
   }
 
-  agregarImagen() ;
+  function agregarTorre() {
+    let contenedor = document.getElementById('tablero');
+    let left = 485;
+
+  
+  for(i = 0; i < 2;i++)
+  {
+    
+     // Crear un elemento de imagen
+     let imagen = document.createElement('img');
+     // Establecer la URL de la imagen
+     imagen.src = './piezas/chess.png';
+      // Opcional: establecer atributos adicionales como el ancho y el alto
+    imagen.width = 50;
+    imagen.height = 50;
+     // Establecer estilos de posicionamiento
+    imagen.style.position = 'absolute';
+    imagen.style.top = '450px'; // Posición vertical
+    imagen.style.left = left.toString() + "px"; 
+
+    left = left + 350;
+   
+    // Adjuntar la imagen al contenedor en el DOM
+    
+    contenedor.appendChild(imagen);
+  }
+  
+  
+  
+    
+  }
+
+  agregarPeon();
+  agregarTorre();
